5259df3c0d9a9ec2c7a5a7fe7fd773e15b6cdaac
Mohammed Salah <Mofree88@outlook.com>
2025-11-03 12:59:24 +0200

    change index title name

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>نظام أرقام الإنتظار</title>
+  <title> نظام أرقام الإنتظار الالى</title>
 
 
   <?php
